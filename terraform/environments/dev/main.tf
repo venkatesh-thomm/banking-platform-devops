b59a095e87c7739c1b07e918e136133572d60819
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-09-19 23:38:41 +0530

    K8 version Updated

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -9,7 +9,7 @@ module "eks" {
   source = "../../modules/eks"
 
   cluster_name       = "banking-dev-eks"
-  kubernetes_version = "1.33"
+  kubernetes_version = "1.34"
 
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
